04dd2df1e56721fa5446ed720fcb7499159189a7
saurabh <saurabhkedar@gmail.com>
2021-12-09 01:16:12 +0530

    rebuilt the file menu

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 //declaring the environment
 process.env.NODE_ENV = "development";
 
 const isDev = process.env.NODE_ENV === "development" ? true : false;
 const isLinux = process.platform === "linux" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
 function buildMainWindow() {
@@ -18,12 +19,38 @@ function buildMainWindow() {
   mainWindow.loadFile("./app/index.html");
 }
 
-app.on("ready", buildMainWindow);
+//status////status////status////status////status////status////status////status////status////status////status////status//
+
+app.on("ready", () => {
+  buildMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("ready", () => (mainWindow = null));
+});
+//status////status////status////status////status////status////status////status////status////status////status////status//
+
+//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    label: "file",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit(),
+      },
+    ],
+  },
+];
+//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu//menu
 
 //mac efficient-mac efficient-window options// //mac efficient-mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options//
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+  if (!isMac) {
     app.quit();
   }
 });
