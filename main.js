cf662adf5ff08a79f2dceb73048e9e61827b852c
saurabh <saurabhkedar@gmail.com>
2021-12-08 22:49:07 +0530

    rebuilt

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow } = require("electron");
 
+//declaring the environment
 process.env.NODE_ENV = "development";
 
-const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isDev = process.env.NODE_ENV === "development" ? true : false;
 const isLinux = process.platform === "linux" ? true : false;
 
 let mainWindow;
-
-function createMainWindow() {
+function buildMainWindow() {
   mainWindow = new BrowserWindow({
-    title: "Shrinkentor",
-    width: 800,
-    height: 500,
+    title: "Shrinkinator",
+    width: 700,
+    height: 400,
     icon: "./assets/icons/Icon_256x256.png",
-    resizable: isDev ? true : false,
   });
 
-  // alternate way..   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile("./app/index.html");
 }
 
-app.on("ready", () => {
-  createMainWindow();
+app.on("ready", buildMainWindow);
 
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
+//mac efficient-mac efficient-window options// //mac efficient-mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options//
 
-  mainWindow.on("ready", () => (mainWindow = null));
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-const menu = [
-  {
-    label: "file",
-    submenu: [
-      {
-        label: "Quit",
-        click: () => app.quit(),
-      },
-    ],
-  },
-];
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    buildMainWindow();
+  }
+});
+//mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options////mac efficient-window options//
+
+app.allowRendererProcessReuse = true;
